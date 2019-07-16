68c0b12d4cdbd7bd2c48fab4bce4c5e4138f2750
Phish Chiang <phish@TrooperFX.com>
2019-07-16 19:25:33 +0800

    Connected to the mongoDB

---
+++ config/db.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+// to access the global variable from config we created
+const config = require('config');
+// so we can use the mongoURI here
+const db = config.get('mongoURI');
+/*
+const connectDB = () => {
+  mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => {
+    console.error(err.message);
+    process.exit(1);
+  });
+};
+*/
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
