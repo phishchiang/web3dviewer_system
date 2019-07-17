d84e0bf576addba1d53539a98c9698aaa207f7cc
Phish Chiang <phish@TrooperFX.com>
2019-07-17 11:10:39 +0800

    Add the user model and validation

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 // Connect Database
 connectDB();
 
+// Init Middleware : we can accept the req.body data
+app.use(express.json({ extended: false }));
+
 // app.get('/', (req, res) => res.send('hello word'));
-app.get('/', (req, res) => 
-  res.json({ msg: 'Welcome to the web 3d viewer API'})
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the web 3d viewer API" })
 );
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
