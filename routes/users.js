4071b72cff206c7d001b97de01a8b6553dfbe87b
Phish Chiang <phish@TrooperFX.com>
2019-07-17 11:53:00 +0800

    Create the JSON web token

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -21,7 +24,7 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     // res.send('Register a user');
     // res.send(req.body);
     const errors = validationResult(req);
@@ -29,7 +32,49 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("passed");
+    // res.send("passed");
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      user = new User({
+        name: name,
+        email: email,
+        password: password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // res.send("User saved");
+      // get the data based on the user id
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
