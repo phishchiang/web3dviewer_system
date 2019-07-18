0f0707a62afe1c6510e21ff8d3290402adfc5d8a
Phish Chiang <phish@TrooperFX.com>
2019-07-18 17:32:31 +0800

    Add the contact form

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -32,7 +32,7 @@ const ContactItem = ({ contact }) => {
       </ul>
       <p>
         <button className="btn btn-dark btn-sm">Edit</button>
-        <button className="btn btn-danger btn-sm">Edit</button>
+        <button className="btn btn-danger btn-sm">Delete</button>
       </p>
     </div>
   );
