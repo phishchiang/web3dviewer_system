9007f2fc94b49154672675d9b56fd7de2cd96688
Phish Chiang <phish@TrooperFX.com>
2019-07-19 17:22:12 +0800

    Add the deploy to heroku part

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -9,15 +10,27 @@ connectDB();
 // Init Middleware : we can accept the req.body data
 app.use(express.json({ extended: false }));
 
+/*
 // app.get('/', (req, res) => res.send('hello word'));
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the web 3d viewer API" })
 );
+*/
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Server static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
